5ca3f63d654faf6d9228c1b107a24c9de2d65464
Ando Deng <a34deng@cpu158.math.private.uwaterloo.ca>
2025-02-05 17:01:34 -0500

    add yaml for hyper configuration

--- mindreadingautobots/src/mindreadingautobots/sparse_majority_jobs.sh
+++ mindreadingautobots/src/mindreadingautobots/sparse_majority_jobs.sh
@@ -1,17 +1,30 @@
+cd mindreadingautobots/src/mindreadingautobots
+conda activate autobots 
+
 python -m main -mode tune -dataset sparse_majority_k5_nbits21_n2000_bf0_seed1234 \
--model_type SAN -noiseless_validation -epochs 200\
+-model_type SAN -noiseless_validation -epochs 1000\
 
 python -m main -mode tune -dataset sparse_majority_k5_nbits21_n2000_bf10_seed1234 \
--model_type SAN -noiseless_validation -epochs 400\
+-model_type SAN -noiseless_validation -epochs 1000\
 
 python -m main -mode tune -dataset sparse_majority_k5_nbits21_n2000_bf20_seed1234 \
--model_type SAN -noiseless_validation -epochs 400\
+-model_type SAN -noiseless_validation -epochs 1000\
 
-# python -m main -mode tune -dataset sparse_majority_k5_nbits21_n2000_bf0_seed1234 \
-# -model_type RNN -noiseless_validation -epochs 100\
+python -m main -mode tune -dataset sparse_majority_k5_nbits21_n2000_bf0_seed1234 \
+-model_type RNN -noiseless_validation -epochs 1000\
 
-# python -m main -mode tune -dataset sparse_majority_k5_nbits21_n2000_bf10_seed1234 \
-# -model_type RNN -noiseless_validation -epochs 300\
+python -m main -mode tune -dataset sparse_majority_k5_nbits21_n2000_bf10_seed1234 \
+-model_type RNN -noiseless_validation -epochs 1000\
 
-# python -m main -mode tune -dataset sparse_majority_k5_nbits21_n2000_bf20_seed1234 \
-# -model_type RNN -noiseless_validation -epochs 400\
+python -m main -mode tune -dataset sparse_majority_k5_nbits21_n2000_bf20_seed1234 \
+-model_type RNN -noiseless_validation -epochs 1000\
+
+python -m main -mode tune -dataset sparse_parity_k4_nbits10_n5000_bf20_seed1234 \
+-model_type RNN -noiseless_validation -epochs 1000\
+
+python -m main -mode tune -dataset sparse_majority_k5_nbits21_n2000_bf35_seed1234 -hyper_config_path /u/a34deng/ResearchDocuments/MindReadingAutobot/mindreadingautobots/hyper_config/xformer_hyper_config.yaml \
+-model_type SAN -noiseless_validation -epochs 1000\ 
+
+python -m main -mode tune -dataset sparse_parity_k4_nbits10_n5000_bf20_seed1234 -hyper_config_path /u/a34deng/ResearchDocuments/MindReadingAutobot/mindreadingautobots/hyper_config/rnn_hyper_config.yaml \
+-model_type RNN -noiseless_validation -epochs 1000\ 
+
